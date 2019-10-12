4eb39577d228c7763fd42abc617e9b51e15d2b6f
jefferyhaupt <43371332+jefferyhaupt@users.noreply.github.com>
2019-10-12 08:16:51 -0400

    Completed checkForWin for all scenarios except for left column and right column

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let boardCells = document.querySelectorAll(".row");
 let turnCounter = 1;
 
-let xMoves = [];
-let oMoves = [];
+let xMoves = "";
+let oMoves = "";
 
 boardCells.forEach(function(boardCell){
   boardCell.addEventListener("click", changeSquare);
@@ -13,21 +13,34 @@ function changeSquare(e){
   if ((turnCounter % 2) != 0 ) {
     clickedSquare.textContent = "X";
     turnCounter++;
-    xMoves.push() = e.target.className;
+    xMoves = xMoves + e.target.className + " ";
     checkForWin("x");
   } else {
     clickedSquare.textContent = "O";
     turnCounter++;
-    oMoves.push() = e.target.className;
+    oMoves = oMoves + e.target.className + " ";
     checkForWin("o");
   }
 }
 
 function checkForWin(moveMade) {
   if (moveMade == "x") {
-    for(let i = 0, i < xMoves.length; i++){
-      
+    if ((xMoves.split("top").length - 1) == 3) {
+      console.log("X Wins! - Top Row");
+    } else if((xMoves.split("bottom").length - 1) == 3) {
+      console.log("X Wins! - Bottom Row");
+    } else if((xMoves.split("middle").length - 1) == 4) {
+      console.log("X Wins! - Middle Row");
+    } else if(((xMoves.split("top left").length - 1) == 1) && ((xMoves.split("middle middle").length - 1) == 1) && ((xMoves.split("bottom right").length - 1) == 1)) {
+      console.log("X Wins Again!");
+    } else if(((xMoves.split("top right").length - 1) == 1) && ((xMoves.split("middle middle").length - 1) == 1) && ((xMoves.split("bottom left").length - 1) == 1)) {
+      console.log("X Wins Again!");
+
     }
+    //console.log(xMoves.split("top").length - 1);
+    /* for(let i = 0, i < xMoves.length; i++){
+
+    } */
   } //end if moveMade = x statement
 }
 
